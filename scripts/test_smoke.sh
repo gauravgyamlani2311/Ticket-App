d2026478004b2f37f14bf4efebb75847494fc737
gauravgyamlani2311 <gauravgyamlanikllk@gmail.com>
2026-04-21 11:17:53 +0530

    update terraform

--- scripts/test_smoke.sh
+++ scripts/test_smoke.sh
@@ -2,11 +2,11 @@
 echo "--- Running Smoke Tests ---"
 
 # Test 1: Web Server Response
-curl -s --head URL="http://54.226.233.59:8081/index.html" | head -n 1 | grep "200" > /dev/null
+curl -s --head URL="http://localhost:8081/index.html" | head -n 1 | grep "200" > /dev/null
 WEB_RES=$?
 
 # Test 2: PHP/DB Connectivity (via the info.php page)
-curl -s http://54.226.233.59:8081/info.php | grep "Connected to Database" > /dev/null
+curl -s http://localhost:8081/info.php | grep "Connected to Database" > /dev/null
 DB_RES=$?
 
 if [ $WEB_RES -eq 0 ] && [ $DB_RES -eq 0 ]; then
